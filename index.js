29788fb75211511d5c9b0553c9042dce4f37a60b
Ryan Cheung <ryan9cheung@gmail.com>
2022-09-18 22:35:34 +0800

    Added Auth

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express()
 const PORT = 8080
 // routes
 const userRouter = require('./routes/userRouter')
+const adminRouter = require('./routes/adminRouter')
 
 app.use(express.json())
 app.use('', userRouter)
+app.use('', adminRouter)
 
 module.exports = app.listen(PORT, () =>
   console.log(`Running API on PORT ${PORT}`)
---
+++ routes/adminRouter.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const checkIfAuthenticated = require('../middleware/middleware')
+const { signInWithEmailAndPassword, getAuth } = require('@firebase/auth')
+const auth = require('../firebase')
+
+app.get('/home', checkIfAuthenticated, async (req, res) => {
+  return res.status(200).send({ message: 'Welcome authenticated user!' })
+})
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  signInWithEmailAndPassword(getAuth(), email, password)
+    .then((userCredential) => {
+      res.status(200).send({ message: 'Successful login!' })
+    })
+    .catch((err) => {
+      res.status(400).send({ message: 'Failed to login' })
+    })
+})
+
+// mocks client request
+app.get('/client', async (_, res) => {
+  const token = await getAuth().currentUser.getIdToken()
+
+  return axios
+    .get('http://localhost:8080/home', {
+      headers: { authorization: `Bearer ${token}` },
+    })
+    .then((response) => {
+      res.status(response.status).send(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(400).send({ message: 'Failed to achieve request!' })
+    })
+})
+
+module.exports = app
